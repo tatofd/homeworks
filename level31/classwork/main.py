d0e09f288f00dd52588ca394e7af30cc877307d7
tato1112 <dzmaargetyvi88@gmail.com>
2026-05-28 21:21:02 +0400

    commit

---
+++ level31/classwork/main.py
@@ -0,0 +1,17 @@
+for i in range(1, 101, 2):
+    print(i)
+
+    num = 0
+
+while num <= 50:
+    print(num)
+    num += 2
+
+
+    age = int(input("შეიყვანე ასაკი: "))
+
+if age >= 18:
+    print("შენ ხარ სრულწლოვანი")
+else:
+    print("შენ არ ხარ სრულწლოვანი")
+
